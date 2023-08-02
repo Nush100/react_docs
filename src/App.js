0164463ff640afefa20f5c39c34641677d44bfca
nushara <nushkavi123@gmail.com>
2023-08-02 14:49:33 +0530

    add styles, board and square

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Board from "./tic-tac-toe/Board";
+
 function App() {
   return (
     <div>
-      
+      <Board/>
     </div>
   );
 }
---
+++ src/tic-tac-toe/Square.js
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+import './style.css';
+
+const Square = () => {
+    const [value, setValue] = useState(null);
+
+    const handleClick = () => {
+        setValue('X');
+    }
+
+    return (
+        <button 
+            className="square"
+            onClick={handleClick}
+        >
+            {value}
+        </button>
+    );
+}
+
+export default Square;
